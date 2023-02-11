48565de164bc17ef62572aed5f1dda8e158bc933
luisferdonoso <luisfer.donoso@gmail.com>
2023-02-11 17:58:28 +0000

    cambios 4

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -14,6 +14,14 @@ const Home = () => {
 			setNewTask("")
 		}
 	}
+
+	function checkTask(index) {
+		//Actualiza check
+		var newTasks = [...tasks]
+		newTasks[index] = {...newTasks[index], done:!newTasks[index].done}
+		setTasks(newTasks)
+	}
+	
 	useEffect (async() =>{
 		var respuesta =await fetch (api)
 		if (respuesta.status == 404) {
@@ -32,20 +40,49 @@ const Home = () => {
 		setTasks(data)
 	},[])
 	
-	function removeTask(index){
-		setTasks(tasks.filter((task, i) => i !== index))
+	const [deleteTask, setDeleteTask] = useState("")
+		
+	async function removeTask(index){
+		console.log(index)
+		var newTasks = [...tasks]
+		newTasks.splice(index, 1)
+		setTasks(newTasks)
 	}
+	async function clearList(){
+		var clearlist = []
+		setTasks(clearlist)
+		let respdelete = await  fetch(api,{
+			method:"DELETE",
+			headers:{
+				"Content-Type":"application/json"
+			}
+		})
+		var respuesta = await fetch(api,{
+				method:"POST",
+				body:JSON.stringify([]),
+				headers:{
+					"Content-Type":"application/json"
+				}
+			})
+
+			respuesta = await fetch(api)
+	
+			console.log("List Deleted")
+
+	}
+
 
 	return (
 		<div className="container-fluid d-flex mt-5 justify-content-center">
 			<ul className="list-group">
 			<li className="list-group-item d-flex justify-content-between align-items-center">
 				<input className="form-control" type="text" onKeyDown={e=>addTask(e)} onChange={e=>setNewTask(e.target.value)} value={newTask} name="task" id="task" />
+				<button className="input-group-text" onClick={()=>clearList()}>Clear All</button>
 			</li>
 				{tasks.map((task, index)=>(
 				<li key={index} className="list-group-item d-flex justify-content-between align-items-center">
 					{task.label}	
-					<span className="badge bg-danger rounded-pill" onClick={() => removeTask(index)}>X</span>
+					<span className="badge bg-danger  rounded-pill" onClick={() => removeTask(index)}>X</span>
 				</li>
 				))}
 					<li className="list-group-item text-center disabled text-muted d-flex justify-content-center align-items-center">
